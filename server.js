1f3c8025931781994de40d4a77627f0abe161f3e
Daniela Remogna <info@fimietta.it>
2016-02-27 12:36:58 +0000

    using fs module for reading a file and sending it back to the client on the / endpoint

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
+var fs = require('fs');
+var server = http.createServer(handler);
 
-var message = 'I am so happy to be part of the NodeJs Workshop';
+var message;
+
 
 function handler(request, response) {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write(message);
-  response.end();
+  var endpoint = request.url;
+
+  switch(endpoint) {
+    case '/node':
+        message = 'NODE Endpoint - I am so happy!';
+        response.write(message);
+        response.end();
+      break;
+
+    case '/girls':
+        message = 'GIRLS Endpoint - I am so happy';
+        response.write(message);
+        response.end();
+      break;
+
+    case '/':
+        response.writeHead(200, {"Content-Type" : "text/html"});
+
+        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+          if(error) {
+            console.log(error);
+            return;
+          }
+
+          response.end(file);
+        });
+
+      break;
+
+    default:
+      message = 'I am so happy to be part of the NodeJs Workshop';
+      response.write(message);
+      response.end();
+
+  }
+
 }
 
 
-var server = http.createServer(handler);
 
 
 server.listen(port, function() {
