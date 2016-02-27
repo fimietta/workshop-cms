8b31ba7691a79d4bfc9343952a8c14beea7c62a4
Daniela Remogna <info@fimietta.it>
2016-02-27 12:02:09 +0000

    added handler for the server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var http = require('http');
-var server = http.createServer();
 var port = process.env.PORT || 3000;
+
+var message = 'I am so happy to be part of the NodeJs Workshop';
+
+function handler(request, response) {
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write(message);
+  response.end();
+}
+
+
+var server = http.createServer(handler);
 
 
 server.listen(port, function() {
